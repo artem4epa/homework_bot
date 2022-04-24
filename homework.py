cdd328a9af4aebebe51e4794c11cb0b23544b625
Артем Чепа <artemchepa@yandex.ru>
2022-04-24 23:09:21 +0300

    rew3

--- homework.py
+++ homework.py
@@ -38,8 +38,7 @@ def send_message(bot, message):
     if response:
         logger.info('Сообщение успешно отправленно.')
         return response
-    else:
-        raise Exception('Сообщение не отправленно.')
+    raise Exception('Сообщение не отправленно.')
 
 
 def get_api_answer(current_timestamp):
@@ -50,42 +49,40 @@ def get_api_answer(current_timestamp):
     headers = HEADERS
     try:
         response = requests.get(ENDPOINT, headers=headers, params=params)
-        if response.status_code == HTTPStatus.OK:
-            response = response.json()
-            return response
-
-        raise HTTPError(
-            "Response: {0}, parameters: {1}".format(
-                response.text, response.request
-            )
+    except Exception:
+        raise ConnectionError('Соединение оборалось.')
+    if response.status_code == HTTPStatus.OK:
+        response = response.json()
+        return response
+    raise HTTPError(
+        "Response: {0}, parameters: {1}".format(
+            response.text, response.request
         )
-    except ConnectionError:
-        raise ConnectionError('Соединение оборвалось')
+    )
 
 
 def check_response(response):
     """Функция проверяет получаемые в запросе данные."""
     if not isinstance(response, dict):
         raise TypeError('Неверный тип данных.')
-    if response.keys() & {'homeworks', 'current_date'}:
-        homeworks = response.get('homeworks')
-    else:
+    if 'current_date' not in response or 'homeworks' not in response:
         raise ValueError('Ошибка')
+    homeworks = response.get('homeworks')
     if not isinstance(homeworks, list):
         raise TypeError('Запрос не вернул список')
     return homeworks
 
 
-def parse_status(homeworks):
+def parse_status(homework):
     """Функция на основе полученных данных формирует сообщение."""
-    if homeworks.keys() & {'homework_name', 'status'}:
-        homework_name = homeworks.get('homework_name')
-        homework_status = homeworks.get('status')
-        verdict = HOMEWORK_VERDICTS[homework_status]
-        return 'Изменился статус проверки работы "{0}". {1}'.format(
-            homework_name, verdict
-        )
-    raise ValueError('Отсутствуют необходимые данные.')
+    if 'homework_name' not in homework and 'status' not in homework:
+        raise ValueError('Отсутствуют необходимые данные.')
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_VERDICTS[homework_status]
+    return 'Изменился статус проверки работы "{0}". {1}'.format(
+        homework_name, verdict,
+    )
 
 
 def check_tokens():
@@ -98,7 +95,7 @@ def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    MESSAGE = ''
+    message_s = ''
     if not check_tokens():
         logger.critical('Переменные окружения недоступны.')
         message = 'Переменные недоступны.'
@@ -109,10 +106,10 @@ def main():
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             if homeworks:
-                message = parse_status(homeworks)
-                if message != MESSAGE:
+                message = parse_status(homeworks[0])
+                if message != message_s:
                     send_message(bot, message)
-                    MESSAGE = message
+                    message_s = message
                     current_timestamp = current_timestamp
 
         except Exception as error:
